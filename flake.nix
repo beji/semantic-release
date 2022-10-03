de7aa4a2795fc6eab2852daa03552fbfd236edf5
Björn Erlwein <bjoern@bjoernerlwein.de>
2022-10-03 21:53:20 +0200

    Fix flake dependencies

--- flake.nix
+++ flake.nix
@@ -6,21 +6,42 @@
 
   outputs = { self, flake-utils, devshell, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system: {
-      defaultPackage = let pkgs = import nixpkgs { inherit system; };
-      in pkgs.rustPlatform.buildRustPackage {
-        pname = "semantic-release";
-        version = "0.2.0";
-        src = ./.;
-        cargoLock.lockFile = ./Cargo.lock;
-      };
-      devShell = let
-        pkgs = import nixpkgs {
-          inherit system;
-
-          overlays = [ devshell.overlay ];
+      defaultPackage =
+        let pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.rustPlatform.buildRustPackage {
+          pname = "semantic-release";
+          version = "0.3.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          buildInputs = [ pkgs.pkg-config pkgs.openssl_3.dev pkgs.openssl_3 pkgs.gcc ];
+          OPENSSL_INCLUDE_DIR = "${pkgs.openssl_3.dev}";
+          OPENSSL_LIB_DIR = "${pkgs.openssl_3.out}/lib";
         };
-      in pkgs.devshell.mkShell {
-        imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
-      };
+      devShell =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+
+            overlays = [ devshell.overlay ];
+          };
+        in
+        pkgs.devshell.mkShell {
+          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+          env = [
+            {
+              name = "RUST_SRC_PATH";
+              value = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+            }
+            {
+              name = "OPENSSL_INCLUDE_DIR";
+              value = "${pkgs.openssl_3.dev}";
+            }
+            {
+              name = "OPENSSL_LIB_DIR";
+              value = "${pkgs.openssl_3.out}/lib";
+            }
+          ];
+        };
     });
 }
