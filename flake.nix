78023798cda5058e3938662b0ee827b3e2feba9b
Björn Erlwein <bjoern@bjoernerlwein.de>
2023-07-05 20:29:23 +0200

    [Semantic release]: Release 1.2.0

--- flake.nix
+++ flake.nix
@@ -64,7 +64,7 @@
         # "basic" rust builds without dream2nix
         packages.default = pkgs.rustPlatform.buildRustPackage {
           pname = "semantic-release";
-          version = "1.1.0";
+          version = "1.2.0";
           src = (inputs.nix-filter.lib {
             root = ./.;
             exclude = [
